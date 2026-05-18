02df2c593a3186538a5319c8606b4ebf5d329c28
Davidson Freitas <156726427+professordavidsonfreitas@users.noreply.github.com>
2026-05-18 08:59:14 -0300

    enable cors2

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ from fastapi.middleware.cors import (
 
 app = FastAPI()
 
+# Configurar CORS
 app.add_middleware(
-
     CORSMiddleware,
-
-    allow_origins=["*"],
-
+    allow_origins=[
+        "https://seu-frontend.com",
+        "https://app.seu-frontend.com",
+    ],
     allow_credentials=True,
-
-    allow_methods=["*"],
-
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
