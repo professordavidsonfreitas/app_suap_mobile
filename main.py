c7829791353e677d2eadce31b1eb1c79e32ad5b4
Davidson Freitas <156726427+professordavidsonfreitas@users.noreply.github.com>
2026-05-18 03:54:27 -0300

    enable cors

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 import os
 
+from fastapi.middleware.cors import (
+    CORSMiddleware
+)
+
 app = FastAPI()
+
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=["*"],
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+)
 
 
 from routers.auth_routes import auth_router
