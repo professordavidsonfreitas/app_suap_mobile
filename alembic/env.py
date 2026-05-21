dd77a0de58ccbb2140a72bba5711f3725d2f3d59
Davidson Freitas <156726427+professordavidsonfreitas@users.noreply.github.com>
2026-05-20 22:12:07 -0300

    migra para postgres

--- alembic/env.py
+++ alembic/env.py
@@ -16,6 +16,17 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # access to the values within the .ini file in use.
 config = context.config
 
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+if DATABASE_URL:
+
+    config.set_main_option(
+        "sqlalchemy.url",
+        DATABASE_URL
+    )
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
